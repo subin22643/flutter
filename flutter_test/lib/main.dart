602dab3784b888539984e975e44e62544541d952
subin22643 <bin577733@naver.com>
2024-04-23 00:03:46 +0900

    코딩애플 5강까지

--- flutter_test/lib/main.dart
+++ flutter_test/lib/main.dart
@@ -11,14 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.star),
-            Icon(Icons.star),
-            Icon(Icons.star),
-          ]
+        body: Scaffold(
+          appBar: AppBar(title: Text('앱제목')),
+          body: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width:60, height:60, color: Colors.blue,
+            ),
+          )
         ),
       )
     );
