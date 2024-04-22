4ec88f4fb24491316348fbad79ea83e50c737348
subin22643 <139415947+subin22643@users.noreply.github.com>
2024-04-22 17:50:03 +0900

    코딩애플 4강까지

---
+++ flutter_test/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+          ]
+        ),
+      )
+    );
+  }
+}
